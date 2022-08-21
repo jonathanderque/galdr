69848f4621e514da2997a5377b4ff91d4713a211
Jonathan Derque <jonathan.derque@gmail.com>
2022-08-21 14:41:16 +0200

    added zig build bundle command

--- build.zig
+++ build.zig
@@ -33,4 +33,54 @@ pub fn build(b: *std.build.Builder) !void {
     const opt_step = b.step("opt", "Run wasm-opt on cart.wasm, producing opt.wasm");
     opt_step.dependOn(&lib.step);
     opt_step.dependOn(&opt.step);
+
+    const description = "\"GALDR - a roguelike full of spells!\"";
+
+    const bundle_html = b.addSystemCommand(&[_][]const u8{
+        "w4",
+        "bundle",
+        "--description",
+        description,
+        "--title",
+        "GALDR",
+    });
+
+    const htmlout = try std.fs.path.join(b.allocator, &.{ prefix, "galdr.html" });
+    defer b.allocator.free(htmlout);
+    bundle_html.addArgs(&.{ "--html", htmlout });
+    bundle_html.addArgs(&.{optout});
+
+    const bundle_linux = b.addSystemCommand(&[_][]const u8{
+        "w4",
+        "bundle",
+        "--description",
+        description,
+        "--title",
+        "GALDR",
+    });
+
+    const linuxout = try std.fs.path.join(b.allocator, &.{ prefix, "galdr.x86_64" });
+    defer b.allocator.free(linuxout);
+    bundle_linux.addArgs(&.{ "--linux", linuxout });
+    bundle_linux.addArgs(&.{optout});
+
+    const bundle_windows = b.addSystemCommand(&[_][]const u8{
+        "w4",
+        "bundle",
+        "--description",
+        description,
+        "--title",
+        "GALDR",
+    });
+
+    const windowsout = try std.fs.path.join(b.allocator, &.{ prefix, "galdr.exe" });
+    defer b.allocator.free(windowsout);
+    bundle_windows.addArgs(&.{ "--windows", windowsout });
+    bundle_windows.addArgs(&.{optout});
+
+    const bundle_step = b.step("bundle", "package galdr-opt.wasm to html/linux/windows");
+    bundle_step.dependOn(&opt.step);
+    bundle_step.dependOn(&bundle_html.step);
+    bundle_step.dependOn(&bundle_linux.step);
+    bundle_step.dependOn(&bundle_windows.step);
 }
