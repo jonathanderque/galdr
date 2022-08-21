6b6c651fa92676df49a1744d57cc143125b5f11e
Jonathan Derque <jonathan.derque@gmail.com>
2022-08-21 14:22:33 +0200

    compilation output goes now to galdr.wasm / added opt build target

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
-    const lib = b.addSharedLibrary("cart", "src/main.zig", .unversioned);
+    const lib = b.addSharedLibrary("galdr", "src/main.zig", .unversioned);
 
     lib.setBuildMode(.ReleaseSmall);
     lib.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
@@ -15,4 +15,22 @@ pub fn build(b: *std.build.Builder) !void {
     lib.export_symbol_names = &[_][]const u8{ "start", "update" };
 
     lib.install();
+
+    const prefix = b.getInstallPath(.lib, "");
+    const opt = b.addSystemCommand(&[_][]const u8{
+        "wasm-opt",
+        "-Oz",
+        "--strip-debug",
+        "--strip-producers",
+        "--zero-filled-memory",
+    });
+
+    opt.addArtifactArg(lib);
+    const optout = try std.fs.path.join(b.allocator, &.{ prefix, "galdr-opt.wasm" });
+    defer b.allocator.free(optout);
+    opt.addArgs(&.{ "--output", optout });
+
+    const opt_step = b.step("opt", "Run wasm-opt on cart.wasm, producing opt.wasm");
+    opt_step.dependOn(&lib.step);
+    opt_step.dependOn(&opt.step);
 }
